2e14ad734c8c0f09dade483126626f37afd58fef
Dirk VL <dirk.vanlaecke@telenet.be>
2020-08-22 17:30:54 +0200

    First test Travis CI with TimerProg

--- src/TestModule.jl
+++ src/TestModule.jl
@@ -1,5 +1,15 @@
 module TestModule
+#version = "0.1.1 from 19-08-2020 21:30" on Julia version 1.5.0
 
-# Write your package code here.
+    using BitblinxData
+    using CryptoCompareData
 
-end
+    uplBitb(timer) = BitblinxData.uplBitblinx(dsn="DB_TST1")
+    uplCrypM(timer) =  CryptoCompareData.uplCrypCom(dsn="DB_TST2")
+    uplCrypH(timer) = CryptoCompareData.uplCrypCom(dsn="DB_TST3",hrmin="hour")
+
+    t1= Timer(uplBitb, 2, interval=600)
+    t2= Timer(uplCrypM, 30, interval=900)
+    t3= Timer(uplCrypH, 500, interval=3600)
+
+end #Module TestModule
